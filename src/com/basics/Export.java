f11bfaf92fa5141157d010d9c02fbe85db44b2f5
wzg <152119156@qq.com>
2019-10-28 22:27:46 +0800

    变量学习

---
+++ src/com/basics/Export.java
@@ -0,0 +1,16 @@
+package com.basics;
+
+public class Export {
+
+    public static void main(String[] args) {
+        int i='d';
+        char c=97;
+        System.out.println("字符d的unicode码是："+i);
+        System.out.println("unicode代表的字符是："+c);
+
+        char char1='\\';
+        char char2='\u2605';
+        System.out.println("输出反斜杠："+char1);
+        System.out.println("输出五角星："+char2);
+    }
+}
---
+++ src/com/basics/Val.java
@@ -0,0 +1,12 @@
+package com.basics;
+
+public class Val {
+    static  int times=24;
+
+    public static void main(String[] args) {
+        int timds=22;
+        System.out.println("现在的时间是："+timds);
+        System.out.println("以前的时间是："+Val.times);
+    }
+
+}
